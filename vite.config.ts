bea2dac7446a6f6cf02e1a36064b3036cb71ff9e
spamiabellasc-collab <spamiabella.sc@gmail.com>
2026-03-03 13:45:16 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: './index.html'
-    }
+  server: {
+    port: 3000
   }
 })
